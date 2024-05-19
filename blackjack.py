40c7a1a55e9ef40f3a180b0e21aca89dde12032d
GeraldJamesLow <123541528+GeraldJamesLow@users.noreply.github.com>
2024-05-20 02:45:00 +0800

    added Hands class to enable support for multiple hands per player. Shifted functions between new classes.

--- blackjack.py
+++ blackjack.py
@@ -1,10 +1,11 @@
 
+import os
 import random
 from typing import Union
 
 class CardManager:
     def __init__(self) -> None:
-        self.cards = set()
+        self.drawn_cards = set()
 
     def generate_card(self) -> tuple[str, Union[int,str]]:
         suit: int = random.choice(['C', 'D', 'H', 'S'])
@@ -12,30 +13,35 @@ class CardManager:
         number: Union[int, str] = random.choice(NUMBERS)
         card = (number, suit)
 
-        if card not in self.cards:
+        if card not in self.drawn_cards:
             return card
-        elif len(self.cards) == 52:
+        elif len(self.drawn_cards) == 52:
             raise Exception("all cards drawn")
         else:
             return self.generate_card()
  
-    def draw_card(self, player) -> None:
+    def draw_card(self) -> None:
         new_card: tuple[str, Union[int,str]] = self.generate_card()
-        self.cards.add(new_card)
-        player.cards.append(new_card)
+        self.drawn_cards.add(new_card)
+        return new_card
 
-class Player():
-    def __init__(self, name) -> None:
-        self.name = name
-        self.cards = []
 
+class Hand():
+    def __init__(self, card_manager: 'CardManager', cards = []) -> None:
+        self.cards = cards
+        self.card_manager = card_manager
+    
+    def draw_card(self) -> None:
+        new_card = self.card_manager.draw_card()
+        self.cards.append(new_card)
+    
     def first_card(self) -> Union[tuple, None]:
         if len(self.cards) > 0:
             return self.cards[0]
         else:
             return None
             # raise Exception("No cards found")
-
+    
     def get_card_value(self, card: tuple[str, Union[list, int]]) -> Union[list[int, int], int]:
         number: Union[int, str] = card[0]
         if number == 'A':
@@ -45,7 +51,7 @@ class Player():
         else:
             return number
     
-    def calculate_hand_total(self, cards: list):
+    def calculate_hand_total(self, cards: list) -> list[int]:
         count = [0]
         for card in cards:
             value: Union[list[int, int], int] = self.get_card_value(card)
@@ -59,14 +65,67 @@ class Player():
                 new_count = list(dict.fromkeys(new_count)) #* remove duplicates from list
                 count = new_count
         return count
+    
+
+class Player():
+    def __init__(self, name, card_manager) -> None:
+        self.name = name
+        self.hands = [Hand(card_manager)]
+        self.money = 500
+    
+    def show_hands(self) -> None:
+        for i, hand in enumerate(self.hands, start = 1):
+            print(f'{self.name} Hand No. {i}: {hand.cards}')
+
+    def add_hand(self) -> None:
+        self.hands.append(Hand(card_manager, cards = []))
+    
+    def draw_card(self, hand_number) -> None:
+        self.hands[hand_number - 1].draw_card() #* hand_number starts from 1
+    
+    #! remember to sanitise input amount to int
+    def transfer_money(self, receiver: 'Player', amount: int) -> None:
+        '''
+        Transfer money from player to receiver
+
+        Args:
+            receiver: Player that is receiving the money
+            amount: amount to be transferred
+        Returns:
+            None
+
+        '''
+        self.money, receiver.money = self.money - amount, receiver.money + amount
+
+def clear_screen() -> None:
+    os.system("cls" if os.name == "nt" else "clear")
+
+def exit_game() -> None:
+    print("Exiting...")
+    exit()
 
 
+if __name__ == '__main__':
+    card_manager = CardManager()
+    Dealer = Player('Dealer', card_manager)
+    Bob = Player("Bob", card_manager)
+    print(Dealer.name, Dealer.hands[0].cards, Dealer.money)
+    Dealer.draw_card(1)
+    print(Dealer.hands[0].cards)
+    Dealer.show_hands()
+    Dealer.add_hand()
 
-card_manager = CardManager()
-Bob = Player("Bob")
-for i in range(5):
-    card_manager.draw_card(Bob)
-    print(card_manager.cards)
-    print(Bob.cards)
+    print(Dealer.hands[0].cards, Dealer.hands[1].cards)
+    Dealer.draw_card(2)
+    Dealer.show_hands() 
 
-print(Bob.calculate_hand_total(Bob.cards))
+    print(Dealer.money, Bob.money)
+    Dealer.transfer_money(Bob, 69)
+    print(Dealer.money, Bob.money)
+    for i in range(2):
+        Bob.draw_card(1)
+        print(card_manager.drawn_cards)
+        Bob.show_hands()
+
+    # print(Bob.calculate_hand_total(Bob.cards))
+    # exit_game()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+
+        card_manager.draw_card(Bob)
+        print(card_manager.cards)
+        print(Bob.cards)
